825577de745617cfb312d062ba2fb083ae44d9a8
hemi_529 <159181198+hemangsharmabtech@users.noreply.github.com>
2024-11-14 11:25:30 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+# tests/test_example.py
+
+# Example of a simple test case
+def test_addition():
+    assert 1 + 1 == 2  # This test will pass
+
+def test_subtraction():
+    assert 3 - 1 == 2  # This test will pass
